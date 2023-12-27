c442491ade03e88b731f3634abc5ea9d6ef7e75e
M Lincon <49597604+mlincon@users.noreply.github.com>
2023-12-27 22:22:26 +0000

    fix mistake on postcreate script for devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,5 +6,5 @@ pre-commit install
 # create and install libraries for pgvector
 conda create -y --name pgvector python=3.11
 conda init
-conda activate pgvector
-pip install -r api/requirements.txt
+conda activate pgvector && \
+    pip install -r pgvector/requirements.txt
